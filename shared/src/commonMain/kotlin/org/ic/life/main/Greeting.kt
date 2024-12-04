c11d9ba3367a5e192ddec2a546753afc1e910fcd
Aurora Study <hardy251223@gmail.com>
2024-12-04 23:56:01 +0700

    feat: test in fleet

--- shared/src/commonMain/kotlin/org/ic/life/main/Greeting.kt
+++ shared/src/commonMain/kotlin/org/ic/life/main/Greeting.kt
@@ -2,8 +2,9 @@ package org.ic.life.main
 
 class Greeting {
     private val platform = getPlatform()
+    private val version: Int = getVersion()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        return "Hello, ${platform.name}! and version: $version"
     }
 }
